8998e634a4ac80bdb560edd30db40b3dc466c974
Chadie Gil <77107717+chadiegil@users.noreply.github.com>
2023-03-15 10:04:49 +0800

    added carousel

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import CatCard from "../../components/catCard/CatCard";
 import Featured from "../../components/featured/Featured";
+import Slide from "../../components/Slide/Slide";
+import TrustedBy from "../../components/trustedBy/TrustedBy";
+import { cards } from "../../data";
 import "./Home.scss";
 
 const Home = () => {
   return (
     <div style={{ fontWeigth: "700" }}>
       <Featured />
+      <TrustedBy />
+      <Slide slidesToShow={5} arrowScroll={5}>
+        {cards.map((card) => (
+          <CatCard item={card} key={card.id} />
+        ))}
+      </Slide>
     </div>
   );
 };
